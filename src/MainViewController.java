1c78513ba521999cf1ea67f2065711c89295fd55
Mateusz <mateusz.pluciennik93@gmail.com>
2016-11-01 19:44:49 +0100

    Added loading picture to ImageView

--- src/MainViewController.java
+++ src/MainViewController.java
@@ -1,15 +1,23 @@
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 
 import java.io.File;
+import java.io.IOException;
 
 public class MainViewController {
     @FXML
-    public void loadPicture() {
+    private ImageView imageView;
+
+    @FXML
+    public void loadPicture() throws IOException {
         final FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open picture");
         fileChooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("Image Files", "*.png", "*jpg"));
         File selectedFile = fileChooser.showOpenDialog(null);
+        Image image = new Image(selectedFile.toURI().toString());
+        imageView.setImage(image);
     }
 }
