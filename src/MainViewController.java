b3a62fe8c798a7eba9d24cd4e9f1a24cf2ca46f6
Mateusz <mateusz.pluciennik93@gmail.com>
2016-11-01 21:28:44 +0100

    Added pixel finding (choosen color)

--- src/MainViewController.java
+++ src/MainViewController.java
@@ -1,14 +1,19 @@
 import javafx.fxml.FXML;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.image.*;
+import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 public class MainViewController {
     @FXML
     private ImageView imageView;
+
+    @FXML
+    private ColorPicker colorPicker;
 
     @FXML
     public void loadPicture() throws IOException {
@@ -17,7 +22,45 @@ public class MainViewController {
         fileChooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("Image Files", "*.png", "*jpg"));
         File selectedFile = fileChooser.showOpenDialog(null);
-        Image image = new Image(selectedFile.toURI().toString());
-        imageView.setImage(image);
+
+        if (selectedFile != null) {
+            Image image = new Image(selectedFile.toURI().toString());
+            imageView.setImage(image);
+        }
     }
+
+    @FXML
+    public void findPixelWithOneThread() {
+        int pixelCounter = 0;
+        System.out.println("One thread here!");
+
+        Image image = imageView.getImage();
+        Color colorPickerValue = colorPicker.getValue();
+
+        PixelReader pixelReader = image.getPixelReader();
+
+        int widith = (int) image.getWidth();
+        int height = (int) image.getHeight();
+
+        WritableImage writableImage = new WritableImage(widith, height);
+        PixelWriter pixelWriter = writableImage.getPixelWriter();
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < widith; x++) {
+                Color color = pixelReader.getColor(x,y);
+                if (Objects.equals(colorPickerValue, color)) {
+                    pixelCounter++;
+                }
+            }
+        }
+
+        System.out.println(pixelCounter);
+
+    }
+
+    @FXML
+    public void  findPixelWithFourThreads() {
+        System.out.println("Four threads here!");
+    }
+
 }
